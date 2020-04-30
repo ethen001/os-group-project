7ea22946cbcb92a4af9ee90701cd681ea4da99b2
hongzhi pan <victoryhongzhi@gmail.com>
2020-04-30 13:11:16 -0400

    format code and update tree

--- list.c
+++ list.c
@@ -1,27 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(){
+int main()
+{
 
 	system("clear");
 
 	//creating file t1.txt.
-	FILE * fp;
+	FILE *fp;
 	fp = fopen("t1.txt", "w+");
-	if(fp == NULL){
+	if (fp == NULL)
+	{
 		printf("Unable to create file.\n");
 	}
-		
+
 	//listing out all content of the current directory.
 	system("ls -l");
-	
+
 	//renaming the t1.txt file to tree.txt.
 	int rn;
 	rn = rename("t1.txt", "tree.txt");
-	if(rn == 0) {
+	if (rn == 0)
+	{
 		printf("\nFile renamed successfully.\n");
-	}else{
+	}
+	else
+	{
 		printf("Error: unable to rename the file\n");
 	}
 
