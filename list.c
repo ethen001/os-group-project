a59b6a0b6d1299f612c0866d1ee74b22a9426a0e
CChariot <lihanzhan@gmail.com>
2020-05-05 15:48:49 -0400

    implemented execlp() method.

--- list.c
+++ list.c
@@ -1,32 +1,52 @@
 #include<stdio.h>
+#include<string.h>
+#include<dirent.h>
 #include<stdlib.h>
 #include<time.h>
+#include<sys/types.h>
+#include<sys/wait.h>
 
-int main(){
-
+int main(int argc, char **argv){
+	int status;	
+	pid_t pid_1, pid_2;	
+	pid_1 = fork();
+	
+	if(pid_1 == 0){//children process
 	system("clear");
+	}
+	else if(pid_1 > 0){//parent process	
 
-	//creating file t1.txt.
+	//listing out all content of the current directory.
+	waitpid(pid_1, &status, 0);
+	pid_2 = fork();
+	if(pid_2 == 0){ //child2
 	FILE * fp;
 	fp = fopen("t1.txt", "w+");
 	if(fp == NULL){
 		printf("Unable to create file.\n");
 	}
-		
-	//listing out all content of the current directory.
-	system("ls -l");
-	
-	//renaming the t1.txt file to tree.txt.
+	execlp("ls","ls","-l", NULL);
+	}
+	else if(pid_2 > 0){ //parent process
+	waitpid(pid_2, &status, 0);
 	int rn;
 	rn = rename("t1.txt", "tree.txt");
-	if(rn == 0) {
+	if(rn == 0){
 		printf("\nFile renamed successfully.\n");
-	}else{
-		printf("Error: unable to rename the file\n");
+	}
+	else{
+		printf("Error: unable to rename the file.\n");
+	}
+	printf("\nAfter update \n");
+	
+	execlp("ls", "ls", "-l", NULL);
 	}
 
-	printf("\nAfter update \n");
-	system("ls -l");
+	else{
+	printf("Failed to fork.");
+	return 1;
+	}	
+	}
 
 	return 0;
 }
