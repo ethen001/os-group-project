912f8aa16caa5e3aa8b400ca1713e912d04a2cdb
ctan01 <48540650+ctan01@users.noreply.github.com>
2020-05-05 18:23:36 -0400

    Update exit.c

--- exit.c
+++ exit.c
@@ -1,33 +1,58 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void exit(){
+int main(){
 
-	
+	int i;
+	char cmd[4096];
+	int cmdHisC =0; 
+	char *cmdHistory;
+	char *cmdsHistory[4];
 
-	//get last 4 commands ands store in to array
-	int last4CmdArray[4] = {}
+
+	while(1) {
+	    fgets(cmd,4096,stdin); //read input from shell
+		if(strcmp(cmd,"") != 0){ 
+	    	if((cmdHistory= strdup(cmd)) != NULL){
+	       		if (cmdsHistory[cmdHisC]){
+	       			free(cmdsHistory[cmdHisC]);
+	       			cmdsHistory[cmdHisC] = cmdHistory;
+	       			cmdHisC++;
+	       		} 
+	    	} 
+	    }
+
+	    else
+	    fprintf(stderr, "Error, Cannot save this command in the history pointer: Out of memory\n");
+
+	    if(cmdHisC>3)
+	        cmdHisC=0;
+	}
+
+	//list last 4 command
+	if(strcmp(argsCmd[0], "history")==0){
+	    for(int n = 0; n<4; n++) {
+	        if(cmdsHistory[n] != NULL)
+	        printf("History command  %d: %s\n", n, cmdsHistory[n]);
+	    }
+	}
+
 
 	// print last 4 commmands
    	printf("\n Last four commands : ");
 
    	system("history 4")
    		
-   	for (int i = 0 ; i < 4 ; i++ ){
-       	printf(" %d " , last4CmdArray[i] , "\n");
-      
-   	}
-
    	printf("\n");
   
-
 	//list all content in current directory
 	printf("\n List of content in cur dir : ");
 
 	system("ls -l");
 
-	return;
+	return 0;
 }
+
 
 
 
