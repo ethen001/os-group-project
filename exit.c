1763af785dd49ca136c04942e3b153f4db7dfb86
hongzhi pan <victoryhongzhi@gmail.com>
2020-05-12 18:00:04 -0400

    version2

--- exit.c
+++ exit.c
@@ -1,27 +1,41 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(int argc, char *argv[]){
-	// print last 4 commmands
-		printf("\nargument: %s, %s, %s", argv[0], argv[1], argv[2]);
-   	printf("\n Last four commands : ");
-
-   	system("history 4");
-   		
-   	printf("\n");
-  
-	//list all content in current directory
-	printf("\n List of content in cur dir : ");
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+void printLastFourCommands(int argc, char *argv[])
+{
+	int count = 0;
+	for (int i = argc - 2; i >= 0; i--) {
+		if (count < 4) {
+			printf("%s\n", argv[i]);
+			++count;
+		}
+		else {
+			break;
+		}
+	}
+}
+void quitOnReturn() {
+	char prev = 0;
+	while (1) {
+		char c = getchar();
+		if (c == '\n' && prev == c)
+		{
+			// double return pressed!
+			break;
+		}
+		prev = c;
+	}
+}
+int main(int argc, char *argv[])
+{
+	printf("\n Last four commands: \n");
+	printLastFourCommands(argc, argv);
+	printf("\n List of content in curr dir");
 	system("ls -l");
-
+	printf("\n");
+	quitOnReturn();
 	return 0;
 }
-
-
-
-
-
-
-
-
